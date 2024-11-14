adaa8009f17727008b2843bcba8afbab2a7fcef1
Tomasz Weglinski <tomasz.weglinski@yougov.com>
2024-11-14 23:43:23 +0100

    eslint; prettier;

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
